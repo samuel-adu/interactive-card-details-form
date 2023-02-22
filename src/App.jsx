68bd2a60d28f21e226c151eb08ec85844ddd2783
Samuel Adu <theadusamuel@gmail.com>
2023-02-22 07:48:43 +0100

    refactor: wrap code in a flex container for desktop layout

--- src/App.jsx
+++ src/App.jsx
@@ -27,88 +27,90 @@ function App() {
 
   return (
     <>
-      <div className="cards">
-        <div className="card card-back">
-          <p className="card-cvc">{cardDetails.cvc}</p>
-        </div>
+      <div className="main">
+        <div className="card-preview">
+          <div className="card card-back">
+            <p className="card-cvc">{cardDetails.cvc}</p>
+          </div>
 
-        <div className="card card-front">
-          <img src={cardLogo} alt="" className="card-logo" />
-          <p className="card-number">{cardDetails.cardNumber}</p>
+          <div className="card card-front">
+            <img src={cardLogo} alt="" className="card-logo" />
+            <p className="card-number">{cardDetails.cardNumber}</p>
 
-          <div className="flex">
-            <p className="card-holder-name">{cardDetails.name}</p>
-            <p className="card-expiry-date">
-              {cardDetails.expMonth}/{cardDetails.expYear}
-            </p>
+            <div className="flex">
+              <p className="card-holder-name">{cardDetails.name}</p>
+              <p className="card-expiry-date">
+                {cardDetails.expMonth}/{cardDetails.expYear}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div>
-        <form onSubmit={handleSubmit} id="card-details-form" className="form">
-          <label htmlFor="name">Cardholder Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            // value={cardDetails.name}
-            onChange={handleChange}
-            placeholder="e.g. Jane Appleseed"
-          />
+        <div className="card-form">
+          <form onSubmit={handleSubmit} id="card-details-form" className="form">
+            <label htmlFor="name">Cardholder Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              // value={cardDetails.name}
+              onChange={handleChange}
+              placeholder="e.g. Jane Appleseed"
+            />
 
-          <label htmlFor="cardNumber">Card Number</label>
-          <input
-            type="text"
-            name="cardNumber"
-            id="cardNumber"
-            // value={cardDetails.cardNumber}
-            onChange={handleChange}
-            placeholder="e.g. 1234 5678 9123 0000"
-            maxLength={16}
-          />
+            <label htmlFor="cardNumber">Card Number</label>
+            <input
+              type="text"
+              name="cardNumber"
+              id="cardNumber"
+              // value={cardDetails.cardNumber}
+              onChange={handleChange}
+              placeholder="e.g. 1234 5678 9123 0000"
+              maxLength={16}
+            />
 
-          <div className="form-control">
-            <div>
-              <label htmlFor="expDate">Exp. Date (MM/YY)</label>
-              <div className="form-control">
+            <div className="form-control">
+              <div>
+                <label htmlFor="expDate">Exp. Date (MM/YY)</label>
+                <div className="form-control">
+                  <input
+                    type="text"
+                    name="expMonth"
+                    id="expMonth"
+                    // value={cardDetails.expMonth}
+                    onChange={handleChange}
+                    placeholder="MM"
+                    maxLength={2}
+                  />
+                  <input
+                    type="text"
+                    name="expYear"
+                    id="expYear"
+                    // value={cardDetails.expYear}
+                    onChange={handleChange}
+                    placeholder="YY"
+                    maxLength={2}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="cardNumber">CVC</label>
                 <input
                   type="text"
-                  name="expMonth"
-                  id="expMonth"
-                  // value={cardDetails.expMonth}
+                  name="cvc"
+                  id="cvc"
+                  // value={cardDetails.cvc}
                   onChange={handleChange}
-                  placeholder="MM"
-                  maxLength={2}
-                />
-                <input
-                  type="text"
-                  name="expYear"
-                  id="expYear"
-                  // value={cardDetails.expYear}
-                  onChange={handleChange}
-                  placeholder="YY"
-                  maxLength={2}
+                  placeholder="e.g. 123"
+                  maxLength={3}
                 />
               </div>
             </div>
 
-            <div>
-              <label htmlFor="cardNumber">CVC</label>
-              <input
-                type="text"
-                name="cvc"
-                id="cvc"
-                // value={cardDetails.cvc}
-                onChange={handleChange}
-                placeholder="e.g. 123"
-                maxLength={3}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Confirm</button>
-        </form>
+            <button type="submit">Confirm</button>
+          </form>
+        </div>
       </div>
     </>
   );
